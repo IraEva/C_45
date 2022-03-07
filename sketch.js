bc4b8e2c703edeebec3f89752fd831833561c6fc
IraEva <86668079+IraEva@users.noreply.github.com>
2022-03-07 16:03:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var Girl1,Girl2,Girl3,Girl4,Girl5,Girl6 ; 
+var strawberry;
+var grape;
+var knife; 
+
+function preload(){
+  Girl_running = loadAnimation("Girl1.png","Girl2.png","Girl3.png","Girl4.png","Girl5.png","Girl6.png");
+  strawberry = loadImage("strawbeery.png");
+  grape = loadImage("grape.png");
+  knife = loadImage("knife.png");
+}
+
+function setup() {
+  createCanvas(600, 600);
+  
+}
+
+function draw() {
+  background(200);
+  
+}
